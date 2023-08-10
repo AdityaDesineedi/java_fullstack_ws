3cbc8ec92ac2d8560dbe2a867cf0a4ca6bf8d0bf
ADITYA DESINEEDI <desineediaditya@gmail.com>
2023-08-09 21:47:28 -0500

    third commit print added sum of two numbers

---
+++ src/main/java/com/amazon/day1/SumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.amazon.day1;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        int sum = a+b;
+        System.out.println(a + "+" + b +"="+ sum);
+    }
+}
