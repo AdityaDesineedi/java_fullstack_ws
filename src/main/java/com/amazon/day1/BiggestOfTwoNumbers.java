054ad6d2dcbf79453f638ca138b6350cf9858172
ADITYA DESINEEDI <desineediaditya@gmail.com>
2023-08-09 21:53:39 -0500

    fourth commit print added biggest of two numbers

---
+++ src/main/java/com/amazon/day1/BiggestOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.amazon.day1;
+
+public class BiggestOfTwoNumbers {
+    public static void main(String[] args){
+        int a = 2;
+        int b=5;
+        System.out.println("Biggest of two is:"+ (a>b?a:b));
+    }
+}
